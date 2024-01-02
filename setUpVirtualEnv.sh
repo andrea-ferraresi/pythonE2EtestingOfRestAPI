5b8c3ff94a02772a9cd984bac77402973ca81702
Andrea <ferrarc@libero.it>
2024-01-02 17:32:50 +0100

    script

---
+++ setUpVirtualEnv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir testEnv
+cd testEnv
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+
+python3 -m pip install requests
+pip install pytest
+pip install pytest-steps
+pip install jsonschema
